3d47852eae552d7059d663d639cc32b84795e1b2
Simon <m13simon@gmail.com>
2018-12-11 12:40:39 +0000

    Icke fungerande metoder skapade i TaBort

--- src/kontaktbok/klasser/TaBortPerson.java
+++ src/kontaktbok/klasser/TaBortPerson.java
@@ -1,4 +1,15 @@
 package kontaktbok.klasser;
 
 public class TaBortPerson {
+    public static void taBort(Personer p){
+        for(int i = 0; i < lista.length; i++) {
+            if (lista.get(i).equals(p)) {
+                lista.remove();
+            }
+        }
+    }
+
+    public static void taBort(int i){
+        lista.remove(i);
+    }
 }
