235b942b6519c0a81b39547fc6379ad040293e34
Simon <m13simon@gmail.com>
2018-12-11 12:56:13 +0000

    Kanske fungerande TaBort

--- src/kontaktbok/klasser/TaBortPerson.java
+++ src/kontaktbok/klasser/TaBortPerson.java
@@ -4,12 +4,28 @@ public class TaBortPerson {
     public static void taBort(Personer p){
         for(int i = 0; i < lista.length; i++) {
             if (lista.get(i).equals(p)) {
-                lista.remove();
+                lista.remove(i);
             }
         }
     }
 
     public static void taBort(int i){
         lista.remove(i);
+    }
+
+    public static void taBortViaNamn(String s){
+        for(int i = 0; i < lista.length; i++) {
+            if (lista.get(i).getNamn().equals(s)) {
+                lista.remove(i);
+            }
+        }
+    }
+
+    public static void taBortViaNummer(Personer p){
+        for(int i = 0; i < lista.length; i++) {
+            if (lista.get(i).getNummer().equals(s)) {
+                lista.remove(i);
+            }
+        }
     }
 }
