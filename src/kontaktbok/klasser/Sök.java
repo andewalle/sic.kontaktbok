101dc979cd909b801c656adb486c7ad164d81d76
andewalle <awtudor@gmail.com>
2018-12-11 15:54:51 +0100

    "Lista och sök påbörjad"

--- src/kontaktbok/klasser/Sök.java
+++ src/kontaktbok/klasser/Sök.java
@@ -1,4 +1,24 @@
 package kontaktbok.klasser;
 
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class Sök {
+
+    public void sökPerson(){
+
+        Lista lista = new Lista();
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Vill du söka på 1. Person eller 2. Företag?");
+
+        int val = sc.nextInt();
+
+        Iterator<Personer> it = new Iterator<Personer>() {
+
+        }
+
+
+    }
 }
