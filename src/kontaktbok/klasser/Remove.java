dff047bb2fb9674c221e2ba556edf2726a8383dd
Simon <m13simon@gmail.com>
2018-12-12 14:23:09 +0000

    Hotfix

--- src/kontaktbok/klasser/Remove.java
+++ src/kontaktbok/klasser/Remove.java
@@ -1,31 +1,4 @@
 package kontaktbok.klasser;
 
-public class TaBort {
-    public static void taBort(Personer p){
-        for(int i = 0; i < lista.length; i++) {
-            if (lista.get(i).equals(p)) {
-                lista.remove(i);
-            }
-        }
-    }
-
-    public static void taBort(int i){
-        lista.remove(i);
-    }
-
-    public static void taBortViaNamn(String s){
-        for(int i = 0; i < lista.length; i++) {
-            if (lista.get(i).getNamn().equals(s)) {
-                lista.remove(i);
-            }
-        }
-    }
-
-    public static void taBortViaNummer(Personer p){
-        for(int i = 0; i < lista.length; i++) {
-            if (lista.get(i).getNummer().equals(s)) {
-                lista.remove(i);
-            }
-        }
-    }
+public class Remove {
 }
