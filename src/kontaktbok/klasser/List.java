4f353289b9023742b9476be4fa137c8190940238
andewalle <awtudor@gmail.com>
2018-12-13 14:34:01 +0100

    "Lists fixed"

--- src/kontaktbok/klasser/List.java
+++ src/kontaktbok/klasser/List.java
@@ -1,5 +1,29 @@
 package kontaktbok.klasser;
 
-public class List {
-    //hej
-}
+import java.util.ArrayList;
+import java.util.Iterator;
+
+    public class Lista {
+        ArrayList<Person> personList = new ArrayList<Person>();
+        public void listPerson(){
+
+            Iterator<Person> it = personList.iterator();
+            int i = 1;
+            while (it.hasNext()){
+                System.out.println(i +". "+it.next().toString());
+                i++;
+            }
+        }
+        ArrayList<Business> businessList = new ArrayList<Business>();
+
+        public void listCompany(){
+
+            Iterator<Business> itc = businessList.iterator();
+            int i = 1;
+            while (itc.hasNext()){
+                System.out.println(i + ". " + itc.next().toString());
+                i++;
+            }
+        }
+    }
+
